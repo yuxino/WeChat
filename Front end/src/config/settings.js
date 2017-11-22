9cc0b2655b2722f27e5d7b1b319c781ef0bc0de7
-A- <1157988103@qq.com>
2017-11-22 15:41:15 +0800

    文件夹名错乱修复

--- Front end/src/config/settings.js
+++
@@ -1,7 +0,0 @@
-export default {
-	socket () {
-		return  (process.env.NODE_ENV === 'testing' || process.env.NODE_ENV === 'production') 
-		? 'http://127.0.0.1:8080'
-		: 'http://www.nbsaw.com'
-	}
-}
